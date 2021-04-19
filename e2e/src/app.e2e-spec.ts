ab8141624d53b81cb77aad3b410598d3bbb2f475
Paula Cure <pau.an.cure@gmail.com>
2021-04-19 19:08:32 -0400

    Travail #17

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,44 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  // it('should display welcome message', async () => {
+  //   await page.navigateTo();
+  //   expect(await page.getTitleText()).toEqual('Gestion des Interventions'+ '\nAccueil'+ '\nDéclarer un problème');
+  // });
+
+  it('#37 | doit afficher le titre du formulaire', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('MonApplication app is running!');
+    expect(await page.getParagraphText()).toEqual('Déclarer un problème');    
+  });
+
+  it('#38 | doit activer le bouton Sauvegarder avec champs valides scénario nominal', async () => {
+    await page.viderToutesLesZones();
+    await page.setChampsValidesScenarioNominal();                              
+    expect(await page.boutonSubmit().isEnabled()).toBeTruthy();
+  });
+
+  it('#39 | doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message TEXTE', async () => {
+    await page.viderToutesLesZones();
+    await page.setChampsValidesScenarioAlternatifParMessageTexte();                              
+    expect(await page.boutonSubmit().isEnabled()).toBeTruthy();
+  });
+
+  it('#40 | doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message courriel', async () => {
+    await page.viderToutesLesZones();
+    await page.setChampsValidesScenarioAlternatifParCourriel();                              
+    expect(await page.boutonSubmit().isEnabled()).toBeTruthy();
+  });
+
+  it('#41 | Zone DESCRIPTION DU PROBLÈME a une bordure VERTE si nombre de caractères suffisant', async () => {
+    await page.viderToutesLesZones();
+    await page.setZoneDescriptionProblemeCaracteresSuffisants();                              
+    expect(await page.obtenirClasseZoneDescriptionProbleme()).toContain('is-valid');
+  });
+  
+  it('#42 | Zone DESCRIPTION DU PROBLÈME a une bordure ROUGE si nombre de caractères insuffisant', async () => {
+    await page.viderToutesLesZones();
+    await page.setZoneDescriptionProblemeCaracteresInuffisants();                              
+    expect(await page.obtenirClasseZoneDescriptionProbleme()).toContain('is-invalid');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,73 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl);
+    return browser.get('/probleme');
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getParagraphText() : Promise<string> {
+    return element(by.css('app-root h5')).getText();
   }
+
+  // Permet de vider toutes les zones.  A appeller dans chaque test.
+  async viderToutesLesZones() : Promise<void> {
+    element(by.id('prenomId')).clear();  
+    element(by.id('nomId')).clear();     
+    // Sélectionner le premier élément dans la zone de liste déroulante (Sélectionner un type de problème (obligatoire))
+    await element(by.id('typeProblemeId')).all(by.tagName('option')).get(0).click();      
+    // Cliquer sur le bouton radio par défaut (Pas de notification)
+    element.all(by.id('notificationId')).get(0).click();
+    element(by.id('courrielId')).clear();
+    element(by.id('courrielConfirmationId')).clear();   
+    element(by.id('telephoneId')).clear();       
+    element(by.id('noUniteId')).clear();
+    element(by.id('descriptionProblemeId')).clear();     
+  }
+
+  // Inscrire tous les renseignements obligatoires pour le scénario de base 
+  async setChampsValidesScenarioNominal() : Promise<void> {
+    element(by.id('prenomId')).sendKeys('tonprenom');
+    element(by.id('nomId')).sendKeys('tonnom');    
+    // Sélectionner le X élément dans la zone de liste déroulante
+    await element(by.id('typeProblemeId')).all(by.tagName('option')).get(2).click();      
+    // Cliquer sur le bouton radio voulu
+    element.all(by.id('notificationId')).get(0).click();  
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+  }
+
+  async setChampsValidesScenarioAlternatifParMessageTexte() : Promise<void> {
+    element(by.id('prenomId')).sendKeys('tonprenom');
+    element(by.id('nomId')).sendKeys('tonnom'); 
+    await element(by.id('typeProblemeId')).all(by.tagName('option')).get(2).click(); 
+    element.all(by.id('notificationId')).get(2).click();
+    element(by.id('telephoneId')).sendKeys('1234567891');         
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+  }
+
+  async setChampsValidesScenarioAlternatifParCourriel() : Promise<void> {
+    element(by.id('prenomId')).sendKeys('tonprenom');
+    element(by.id('nomId')).sendKeys('tonnom'); 
+    await element(by.id('typeProblemeId')).all(by.tagName('option')).get(2).click(); 
+    element.all(by.id('notificationId')).get(1).click();
+    element(by.id('courrielId')).sendKeys('courriel@test.com');  
+    element(by.id('courrielConfirmationId')).sendKeys('courriel@test.com');        
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+  }
+
+  // Permet d'obtenir toutes les propriétés et leurs valeurs du bouton Sauvegarder
+  boutonSubmit() : ElementFinder { 
+    return element(by.buttonText('Sauvegarder'));
+  } 
+
+  async setZoneDescriptionProblemeCaracteresInuffisants() : Promise<void> {
+    element(by.id('descriptionProblemeId')).sendKeys('XX');
+  }
+
+  async setZoneDescriptionProblemeCaracteresSuffisants() : Promise<void> {
+    element(by.id('descriptionProblemeId')).sendKeys('XXXXXX');
+  }
+
+  obtenirClasseZoneDescriptionProbleme()   { 
+    return element(by.id('descriptionProblemeId')).getAttribute("class");
+  } 
 }
